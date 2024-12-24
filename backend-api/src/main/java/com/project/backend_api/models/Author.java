774f6018f7048c44085069f4ef7e61fe58ad01a5
HristiyanBratov <hristiyanbratov@gmail.com>
2024-12-24 17:46:32 +0200

    Create Book and Author entities

---
+++ backend-api/src/main/java/com/project/backend_api/models/Author.java
@@ -0,0 +1,68 @@
+package com.project.backend_api.models;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity(name = "authors")
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private int id;
+
+    @Column(name = "First_Name", nullable = false)
+    private String firstName;
+
+    @Column(name = "Last_Name", nullable = false)
+    private String lastName;
+
+    @Column(name = "Nationality")
+    private String nationality;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books;
+
+    public Author() {}
+
+    public Author(String firstName, String lastName, String nationality) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.nationality = nationality;
+
+        this.books = new HashSet<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+}
--- backend-api/src/main/java/com/project/backend_api/models/Book.java
+++ backend-api/src/main/java/com/project/backend_api/models/Book.java
@@ -1,6 +1,102 @@
 package com.project.backend_api.models;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity(name = "books")
 public class Book
 {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private int id;
+
+    @Column(name = "Title", nullable = false)
+    private String title;
+
+    @Column(name = "ISBN", nullable = false, unique = true, length = 13)
+    private String isbn;
+
+    private String genre;
+
+    @Column(name = "Publication_Date", nullable = false)
+    private LocalDate publicationDate;
+
+    @Column(name = "Copiews_Available", nullable = false)
+    private int copiesAvailable;
+
+    @ManyToMany
+    @JoinTable(name = "book_author", joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors;
+
+    public Book() {}
+
+    public Book(String title, String isbn, LocalDate publicationDate, String genre, int copiesAvailable) {
+        this.title = title;
+        this.isbn = isbn;
+        this.publicationDate = publicationDate;
+        this.genre = genre;
+        this.copiesAvailable = copiesAvailable;
+
+        this.authors = new HashSet<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public LocalDate getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(LocalDate publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public int getCopiesAvailable() {
+        return copiesAvailable;
+    }
+
+    public void setCopiesAvailable(int copiesAvailable) {
+        this.copiesAvailable = copiesAvailable;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
 }
