e8ac61236db57d0db489cc54420d783f5b1d00dd
Руслан Каписъски <ruslank1@abv.bg>
2025-01-08 18:37:10 +0200

    Implemented tests to the custom methods in Library Branch Repository layer

--- backend-api/src/test/java/com/project/backend_api/repository/BookRepositoryTest.java
+++ backend-api/src/test/java/com/project/backend_api/repository/BookRepositoryTest.java
@@ -3,6 +3,7 @@ package com.project.backend_api.repository;
 import com.project.backend_api.models.Book;
 import com.project.backend_api.models.Genre;
 import com.project.backend_api.repositories.BookRepository;
+import com.project.backend_api.repositories.GenreRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,17 +20,23 @@ public class BookRepositoryTest {
 
     @Autowired
     private BookRepository bookRepository;
+    private GenreRepository genreRepository;
     Book book;
+    Genre genre;
 
     @BeforeEach
     void setUp() {
+       genre.setName("Novel");
+       genreRepository.save(genre);
         //    public Book(String title, String isbn, LocalDate publicationDate, Genre genre, Integer copiesAvailable) {LocalDate.parse("1961, 11, 10"),
-        book = new Book("Catch 22","0205080057", LocalDate.parse("1961-10-11"),new Genre("Novel"),12);
+        book = new Book("Catch 22","0205080057", LocalDate.parse("1961-10-11"),genre,12);
         bookRepository.save(book);
     }
 
     @AfterEach
     void tearDown() {
+        genre = null;
+        genreRepository.deleteAll();
         book = null;
         bookRepository.deleteAll();
     }
@@ -40,8 +47,7 @@ public class BookRepositoryTest {
     void testFindByBookTitle_Found() {
        List<Book> bookList = bookRepository.findByTitle("Catch 22");
        assertThat(bookList.get(0).getId()).isEqualTo(book.getId());
-       assertThat(bookList.get(0).getGenre())
-        .isEqualTo(book.getGenre());
+       assertThat(bookList.get(0).getTitle()).isEqualTo(book.getTitle());
     }
 
     //Failure case
---
+++ backend-api/src/test/java/com/project/backend_api/repository/LibraryBranchRepositoryTest.java
@@ -0,0 +1,62 @@
+package com.project.backend_api.repository;
+
+
+import com.project.backend_api.models.Book;
+import com.project.backend_api.models.LibraryBranch;
+import com.project.backend_api.repositories.BookRepository;
+import com.project.backend_api.repositories.LibraryBranchRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
+public class LibraryBranchRepositoryTest {
+    @Autowired
+    private LibraryBranchRepository libraryBranchRepository;
+    private LibraryBranch libraryBranch;
+
+    //Optional<LibraryBranch> findByBranchName(String branchName);
+    @BeforeEach
+    public void setUp() {
+        libraryBranch = new LibraryBranch("Sofia_Branch", "Sofia", "0884543231", "10AM:18PM");
+        libraryBranchRepository.save(libraryBranch);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        libraryBranch = null;
+        libraryBranchRepository.deleteAll();
+    }
+
+
+    //Success case
+    @Test
+    void findByBranchName_Found() {
+        List<LibraryBranch> libraryBranches = libraryBranchRepository
+                .findByBranchName("Sofia_Branch")
+                .stream()
+                .toList();
+        assertThat(libraryBranches.get(0).getBranchName()).isEqualTo("Sofia_Branch");
+        assertThat(libraryBranches.get(0).getBranchName())
+                .isEqualTo(libraryBranch.getBranchName());
+    }
+
+
+    @Test
+    void findByBranchName_NotFound() {
+        List<LibraryBranch> libraryBranches = libraryBranchRepository
+                .findByBranchName("Plovdiv")
+                .stream()
+                .toList();
+        assertThat(libraryBranches.isEmpty()).isTrue();
+
+
+    }
+
+}
